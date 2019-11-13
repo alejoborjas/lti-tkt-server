a8d6f5daa1bf7423b8477bd58489ee197c9929b5
mosjeffinately <mightymosjeff@gmail.com>
2019-11-13 11:28:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+
 // Programs
 app.get('/api/programs', (req, res) => {
     // TODO: get list of programs
